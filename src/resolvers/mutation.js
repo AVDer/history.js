a35b06c0fa6bd9c5f48b981491bd1a083171dc54
Andrii Derevianko <aderevianko@cinemo.com>
2020-03-12 18:09:58 +0100

    SignIn mutation added

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,37 +1,52 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { AuthenticationError,
-    ForbiddenError
-} = require('apollo-server-express');
+const {AuthenticationError, ForbiddenError} = require('apollo-server-express');
 require('dotenv').config();
 
 var JWT_SECRET = 'Secret56';
 
-exports.newLeader = async (parent, args, { models }) => {
-    console.log(args);
-    return await models.Leader.create({
-        nameLatin: args.name,
-        nameOriginal: args.name
-    });
+exports.newLeader = async (parent, args, {models}) => {
+  console.log(args);
+  return await models.Leader.create(
+      {nameLatin: args.name, nameOriginal: args.name});
 };
 
-exports.signUp = async (parent, { username, email, password }, { models }) => {
+exports.signUp = async (parent, {username, email, password}, {models}) => {
+  // normalize email address
+  email = email.trim().toLowerCase();
+  // hash the password
+  const hashed = await bcrypt.hash(password, 10);
+  try {
+    const user = await models.User.create({username, email, password: hashed});
+
+    // create and return the json web token
+    return jwt.sign({id: user._id}, JWT_SECRET);
+  } catch (err) {
+    console.log(err);
+    // if there's a problem creating the account, throw an error
+    throw new Error('Error creating account');
+  }
+};
+
+exports.signIn = async (parent, {username, email, password}, {models}) => {
+  if (email) {
     // normalize email address
     email = email.trim().toLowerCase();
-    // hash the password
-    const hashed = await bcrypt.hash(password, 10);
-    try {
-        const user = await models.User.create({
-            username,
-            email,
-            password: hashed
-        });
+  }
 
-        // create and return the json web token
-        return jwt.sign({ id: user._id }, JWT_SECRET);
-    } catch (err) {
-        console.log(err);
-        // if there's a problem creating the account, throw an error
-        throw new Error('Error creating account');
-    }
+  const user = await models.User.findOne({$or: [{email}, {username}]});
+
+  // if no user is found, throw an authentication error
+  if (!user) {
+    throw new AuthenticationError('Error signing in');
+  }
+
+  // if the passwords don't match, throw an authentication error
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    throw new AuthenticationError('Error signing in');
+  }
+
+  // create and return the json web token
+  return jwt.sign({id: user._id}, JWT_SECRET);
 };
